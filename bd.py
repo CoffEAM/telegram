fab1f75c49c08f9f85acf1196b150b321823568f
lenya_official <93034971+CoffEAM@users.noreply.github.com>
2024-04-24 16:26:35 +0200

    Add files via upload

--- bd.py
+++ bd.py
@@ -4,28 +4,63 @@ connection = sqlite3.connect('DataBaze')
 cursor = connection.cursor()
 
 
-def get_lesson_id(class_num: int, profile_id: int) -> list:
-    data = cursor.execute('SELECT lesson_id FROM timetable WHERE class = (?) AND profile_id = (?) ORDER BY num ASC ',
-                          (class_num, profile_id,))
+def get_lesson_id(class_num: int, profile_id: int, day: str) -> list:
+    data = cursor.execute('SELECT lesson_id FROM timetable WHERE class = (?) AND profile_id = (?) AND day = (?) ORDER BY num ASC',
+                          (class_num, profile_id, day))
     data = data.fetchall()
-    print(data)
+    nums = get_num_lesson(class_num, profile_id, day)
+    dicts = {}
+
+    for i in range(len(data)):
+        dicts[nums[i][0]] = int(data[i][0])
+
+    return dicts
+
+
+def get_num_lesson(class_num: int, profile_id: int, day: str) -> dict:
+    data = cursor.execute('SELECT num FROM timetable WHERE class = (?) AND profile_id = (?) AND day = (?) ORDER BY num ASC',
+                          (class_num, profile_id, day)).fetchall()
+
     return data
 
 
-def get_lesson(class_num: int, profile_id: int) -> list:
-    lesson_list_obj = get_lesson_id(class_num, profile_id)
+def get_lesson(class_num: int, profile_id: int, day: str) -> list:
+    lesson_list_obj = list(dict(get_lesson_id(class_num, profile_id, day)).values())
     lessons = []
     for i in lesson_list_obj:
-        data = cursor.execute('SELECT name FROM lessons WHERE id = (?)',(i[0],)).fetchall()
+        data = cursor.execute('SELECT name FROM lessons WHERE id = (?)', (i,)).fetchall()
         lessons.append(data[0][0])
 
-    print(lessons)
     return lessons
+
+
+def get_id_of_time(class_num: int, profile_id: int, day: str) -> list:
+    data = cursor.execute('SELECT time_id FROM timetable WHERE class = (?) AND profile_id = (?) AND day = (?) ORDER BY num ASC',
+                          (class_num, profile_id, day)).fetchall()
+
+    return data
+
+
+def get_time_of_lesson(class_num: int, profile_id: int, day: str) -> list:
+    time_list = get_id_of_time(class_num, profile_id, day)
+    times = []
+    for i in time_list:
+        data = cursor.execute('SELECT time FROM time WHERE id = (?)', (i[0],)).fetchall()
+        times.append(data[0][0])
+
+    return times
+
+
+def get_profile_id_by_name(name_of_profile: str) -> list:
+    data = cursor.execute('SELECT id FROM profiles WHERE name = (?)', (name_of_profile, )).fetchall()
+
+    return data
 
 
 def get_profile_id(class_num: int) -> list:
     data = cursor.execute('SELECT profile_id FROM timetable WHERE class = (?) GROUP BY profile_id', (class_num,))
     data = data.fetchall()
+
     return data
 
 
@@ -44,5 +79,9 @@ def get_profiles(class_num: int) -> list:
     return data
 
 
-print(get_lesson(10, 2))
+print(get_lesson_id(10, 2, 'пн'))
+print(get_lesson(10, 2, 'пн'))
+print(get_time_of_lesson(10, 2, 'пн'))
+
+
 
